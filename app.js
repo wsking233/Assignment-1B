f5100b35fe8625829874f34b1db39bb8d3a67652
wsking233 <2310016713@qq.com>
2022-05-04 05:11:25 +0800

    build

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const { connect } = require('mongoose');
 const connectDB = require('./config/db');
+require("dotenv").config();
 
 const app = express();
 
@@ -10,10 +11,20 @@ connectDB();
 
 app.get('/',(req,res) => res.send('Hello World!'));
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 3000;
 
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+});
+
+//return the port number
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-
 
 
