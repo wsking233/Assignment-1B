c254b556bf838608c6546da206405eebdb911d58
RuiyangXu <332971895@qq.com>
2022-05-19 20:58:25 +0800

    basic function Connect to MongoDB success

---
+++ frontend/routes/routes.js
@@ -0,0 +1,22 @@
+const { request, response } = require('express');
+const express = require('express');
+const router = express.Router();
+const articleTemplateCopy = require('../models/articleModels');
+
+router.post('/submit', (request, response) => {
+    //request the data via post
+    const articleSubmit = new articleTemplateCopy({
+        title: request.body.title,
+        author: request.body.author,
+        year: request.body.year,
+        source: request.body.source,
+        url: request.body.url,
+    })
+    articleSubmit.save()
+
+    .catch(error =>{
+        response.json(error);
+    })
+})
+
+module.exports = router 
