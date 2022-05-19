3eb918ff260198850588edc5f5b810606302e089
RuiyangXu <332971895@qq.com>
2022-05-19 12:02:18 +0800

    imporvent the submit page

--- frontend/src/components/submit.js
+++ frontend/src/components/submit.js
@@ -5,14 +5,51 @@ class submit extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchQuery: "",
+      title: "",
+      author: "",
+      year: "",
+      source: "",
+      url: ""
     };
   }
-  //handler for Search Bar
-  handleInputChanged(event) {
+  //handler for title input
+  handleInputTitle(event) {
     this.setState({
-      searchQuery: event.target.value,
+      title: event.target.value,
     });
+  }
+  //handler for author input
+  handleInputAuthor(event) {
+    this.setState({
+      author: event.target.value,
+    });
+  }
+    //handler for year input
+  handleInputYear(event) {
+    this.setState({
+      year: event.target.value,
+    });
+  }
+    //handler for source input
+  handleInputSource(event) {
+    this.setState({
+      source: event.target.value,
+    });
+  }
+    //handler for url input
+  handleInputUrl(event) {
+    this.setState({
+      url: event.target.value,
+    });
+  }
+
+  handleSubmit(event){
+    alert("Title is: "+this.state.title);
+    alert("Author is: "+this.state.title);
+    alert("Year is: "+this.state.title);
+    alert("Source is: "+this.state.title);
+    alert("URL is: "+this.state.title);
+    event.preventDefault();
   }
   render() {
     return (
@@ -28,41 +65,48 @@ class submit extends Component {
           <h1 className="title">Submit an Article</h1>
           <br />
           {/*title lable*/}
+          <form onSubmit={this.handleSubmit.bind(this)}>
           <label>Title: </label>
           <input  
           className="textbox" 
-          onChange={this.handleInputChanged.bind(this)} 
+          onChange={this.handleInputTitle.bind(this)} 
+          value={this.state.title}
            />
           <br /><br />
           {/*author lable*/}
           <label>Author: </label>
           <input 
           className="textbox"
-          onChange={this.handleInputChanged.bind(this)}
+          onChange={this.handleInputAuthor.bind(this)}
+          value={this.state.author}
            />
           <br /><br />
           {/*year lable*/}
           <label>Year: </label>
           <input
           className="textbox" 
-          onChange={this.handleInputChanged.bind(this)} 
+          onChange={this.handleInputYear.bind(this)} 
+          value={this.state.year}
            />
           <br /><br />
           {/*source lable*/}
           <label>Source: </label>
           <input className="textbox"
-          onChange={this.handleInputChanged.bind(this)}
+          onChange={this.handleInputSource.bind(this)}
+          value={this.state.source}
            /> 
           <br /><br />  
           {/*url lable*/}       
           <label>Url: </label>
           <input  
           className="textbox" 
-          onChange={this.handleInputChanged.bind(this)} 
+          onChange={this.handleInputUrl.bind(this)} 
+          value={this.state.url}
            />
-        </div>
-        <div>
-            <button className="btn">Submit</button>
+          <div>
+          <button className="btn">Submit</button>
+          </div>
+          </form>
         </div>
       </div>
     );
